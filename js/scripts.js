51b592a6356af52a53e38010ec309aa1557703f3
jfdinosaurio <77809118+jfdinosaurio@users.noreply.github.com>
2021-03-02 20:49:06 -0300

    navbar update
    -Added styles to links
    -Added user icon with dropdown
    -Added notification icons that can change using JS

---
+++ js/scripts.js
@@ -0,0 +1,17 @@
+
+
+notifications_handler(false);
+
+
+function notifications_handler(notifications){
+    let nn = document.getElementById("no_notifications");
+    let wn = document.getElementById("with_notifications");
+    if (notifications) {
+            nn.style.display = "none";     
+            wn.style.display = "block";
+        } 
+    else {
+            nn.style.display = "block";     
+            wn.style.display = "none";
+    }
+}
