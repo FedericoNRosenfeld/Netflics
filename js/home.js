65c436d2b101e9d1cc7ad7875f86de7ddf950f19
FedericoNRosenfeld <fn.rosenfeld@gmail.com>
2021-03-16 21:03:09 -0300

    Premier video
    -Added New video with some info at the top of the page
    -some bugfix

--- js/home.js
+++ js/home.js
@@ -19,3 +19,31 @@ document.getElementById("search_icon_mobile").addEventListener('click', function
     
 
 
+$(document).ready( function(){
+    $(".data_container").height($(".promo_container").height());
+});
+
+
+$(".addList_btn1").click( function(){
+    $(".addList_btn1").toggleClass("item_added_list_btn");
+    setTimeout(function(){
+        $(".addList_btn2").toggleClass("hide_element");
+        $(".addList_btn1").toggleClass("hide_element");
+
+    },200);
+});
+
+$(".addList_btn2").click( function(){
+    $(".addList_btn1").toggleClass("item_added_list_btn");
+    $(".addList_btn2").toggleClass("hide_element");
+
+    $(".addList_btn1").toggleClass("hide_element");
+});
+
+
+
+$(document).ready(function(){
+    $('[data-toggle="tooltip"]').tooltip();   
+});
+
+
